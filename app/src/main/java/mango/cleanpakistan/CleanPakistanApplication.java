f7a4181dc63bace1e379736ce4cd711fdd84ad52
Aizaz Ahmad <aizazahmad56@gmail.com>
2015-02-27 15:07:30 +0500

    Notifications
    Notifications are opening in the right activity.

--- app/src/main/java/mango/cleanpakistan/CleanPakistanApplication.java
+++ app/src/main/java/mango/cleanpakistan/CleanPakistanApplication.java
@@ -7,6 +7,7 @@ import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
+import com.parse.PushService;
 import com.parse.SaveCallback;
 
 /**
@@ -18,7 +19,6 @@ public class CleanPakistanApplication extends Application {
         super.onCreate();
         Parse.initialize(this, "QYP3WEGkoWbgGFcjUVO6n4x18s7pLziFbHJHZcDf", "yKm8tqxzFIkXnmWlY9jHISd6wPbTD9zcSS13ysdo");
         ParseInstallation.getCurrentInstallation().saveInBackground();
-
         ParsePush.subscribeInBackground("", new SaveCallback() {
             @Override
             public void done(ParseException e) {
